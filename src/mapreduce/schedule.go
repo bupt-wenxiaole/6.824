c39e86c88bcd4b29768b154e905164e830e8f6f6
bupt-wenxiaole <xiaole.wen@bupt.edu.cn>
2017-05-27 18:04:29 +0800

    sheduler

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,6 +2,8 @@ package mapreduce
 
 import "fmt"
 import "sync"
+import "fmt"
+import "math"
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -12,6 +14,10 @@ import "sync"
 // suitable for passing to call(). registerChan will yield all
 // existing registered workers (if any) and new ones as they register.
 //
+type workerStatus struct {
+    nTasks int
+    concurrent int
+}
 func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, registerChan chan string) {
 
 	var ntasks int
@@ -29,26 +35,53 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 
 
 	//master维护的是worker的各种信息，scheduler负责从worker列表中根据算法来挑选worker执行task
+	//执行了两次schedule()，一次是mapphase,一次是reducephase，意味着有两个scheduler
+	//两个scheduler之间的scheduler是顺序执行的，一个scheduler内调度的各个task是并行的，注意这里面的并发关系
 	// All ntasks tasks have to be scheduled on workers, and only once all of
 	// them have been completed successfully should the function return.
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	// shecheduler启动一个go去持续读取registerChan并维护一个当前最近的worker列表
-	workerList []string 
-	var listMutex = &sync.Mutex{}
+	// shecheduler启动一个go去持续读取registerChan并把这个新的worker添加到map里，并将这个key对应的两个状态参数初值
+	// 设置为0
+	workerStatus := make(map[string] *workerStatus)  
+	var mapMutex = &sync.Mutex{}
 	go func() {
 		for {
 			newWorkerName := <- registerChan
-			listMutex.Lock()
-			workerList := append(workerList, newWorkerName)
-			listMutex.Unlock()
+			mapMutex.Lock()
+			workerStatus[newWorkerName] = &workerStatus{0, 0}
+			mapMutex.Unlock()
 		}
-	//接下来shedule根据自己的phase来将这些任务进行调度，对每个map任务，读取一遍workerlist从上面RPC获取参数，根据参数做负载均衡执选出
-	//对应的worker调用dotask
-
 	}
 
+	
+	switch phase {
+	case mapPhase: 
+		for i, f := range mapFiles {
+			//根据负载均衡算法选择合适的worker并且更新worker的状态
+			var workerSelected string
+			minTasks := 0x7fffffff
+			for wk, wks := range workerStatus {
+				if wks.concurrent < 1 && wks.nTasks < minTasks {
+					minTasks = wks.nTasks
+					workerSelected = wk
+				}
+			}
+			//注意DoTask里面封装的是rpc.call()，call()是同步调用，远端执行结束后才会进行下一步
+			//这边scheduler使用goroutine并行去调用domap，scheduler可以使用channel构造wait和waitgroup来等待goroutine的结束
+			func (wk *Worker) DoTask(arg *DoTaskArgs, _ *struct{}) error
+			go DoTask()
+	}
+			doMap(jobName, i, f, mr.nReduce, mapF)
+		}
+	case reducePhase:
+		for i := 0; i < mr.nReduce; i++ {
+			doReduce(mr.jobName, i, mergeName(mr.jobName, i), len(mr.files), reduceF)
+		}
+	}
+	
 	fmt.Printf("Schedule: %v phase done\n", phase)
+
 }
--- src/mapreduce/worker.go
+++ src/mapreduce/worker.go
@@ -5,139 +5,135 @@ package mapreduce
 //
 
 import (
-	"fmt"
-	"log"
-	"net"
-	"net/rpc"
-	"os"
-	"sync"
+    "fmt"
+    "log"
+    "net"
+    "net/rpc"
+    "os"
+    "sync"
 )
 
 // Worker holds the state for a server waiting for DoTask or Shutdown RPCs
 type Worker struct {
-	sync.Mutex  //注意这种写法，使用了Golang的匿名字段和内嵌结构体
-	//Go中的继承是通过内嵌或组合来实现的
+    sync.Mutex  //注意这种写法，使用了Golang的匿名字段和内嵌结构体
+    //Go中的继承是通过内嵌或组合来实现的
 
-	name       string
-	Map        func(string, string) []KeyValue
-	Reduce     func(string, []string) string
-	nRPC       int // quit after this many RPCs; protected by mutex  在nRPC次RPC调用后worker退出，
-	//如果初值传入-1的话则意味着worker可以无限次执行RPC调用，注意这种写法
-	nTasks     int // total tasks executed; protected by mutex
-	concurrent int // number of parallel DoTasks in this worker; mutex
-	l          net.Listener
+    name       string
+    Map        func(string, string) []KeyValue
+    Reduce     func(string, []string) string
+    nRPC       int // quit after this many RPCs; protected by mutex  在nRPC次RPC调用后worker退出，
+    //如果初值传入-1的话则意味着worker可以无限次执行RPC调用，注意这种写法
+    nTasks     int // total tasks executed; protected by mutex
+    concurrent int // number of parallel DoTasks in this worker; mutex
+    l          net.Listener
 }
 
-type StatusReply struct {
-	nTasks int
-	concurrent int
-}
 
 // DoTask is called by the master when a new task is being scheduled on this
 // worker.
 func (wk *Worker) DoTask(arg *DoTaskArgs, _ *struct{}) error {
-	fmt.Printf("%s: given %v task #%d on file %s (nios: %d)\n",
-		wk.name, arg.Phase, arg.TaskNumber, arg.File, arg.NumOtherPhase)
+    fmt.Printf("%s: given %v task #%d on file %s (nios: %d)\n",
+        wk.name, arg.Phase, arg.TaskNumber, arg.File, arg.NumOtherPhase)
 
-	wk.Lock()
-	wk.nTasks += 1
-	wk.concurrent += 1
-	nc := wk.concurrent
-	wk.Unlock()
+    wk.Lock()
+    wk.nTasks += 1
+    wk.concurrent += 1
+    nc := wk.concurrent
+    wk.Unlock()
 
-	if nc > 1 {
-		// schedule() should never issue more than one RPC at a
-		// time to a given worker.
-		log.Fatal("Worker.DoTask: more than one DoTask sent concurrently to a single worker\n")
-	}
+    if nc > 1 {
+        // schedule() should never issue more than one RPC at a
+        // time to a given worker.
+        log.Fatal("Worker.DoTask: more than one DoTask sent concurrently to a single worker\n")
+    }
 
-	switch arg.Phase {
-	case mapPhase:
-		doMap(arg.JobName, arg.TaskNumber, arg.File, arg.NumOtherPhase, wk.Map)
-	case reducePhase:
-		doReduce(arg.JobName, arg.TaskNumber, mergeName(arg.JobName, arg.TaskNumber), arg.NumOtherPhase, wk.Reduce)
-	}
+    switch arg.Phase {
+    case mapPhase:
+        doMap(arg.JobName, arg.TaskNumber, arg.File, arg.NumOtherPhase, wk.Map)
+    case reducePhase:
+        doReduce(arg.JobName, arg.TaskNumber, mergeName(arg.JobName, arg.TaskNumber), arg.NumOtherPhase, wk.Reduce)
+    }
 
-	wk.Lock()
-	wk.concurrent -= 1
-	wk.Unlock()
+    wk.Lock()
+    wk.concurrent -= 1
+    wk.Unlock()
 
-	fmt.Printf("%s: %v task #%d done\n", wk.name, arg.Phase, arg.TaskNumber)
-	return nil
+    fmt.Printf("%s: %v task #%d done\n", wk.name, arg.Phase, arg.TaskNumber)
+    return nil
 }
 //this func is for getting the worker's status parameters
-func (wk *Worker) GetStatus(_ *struct{}, res *StatusReply) error{
-	fmt.Printf("%s: schedule() ask me the status parameters\n", wk.name)
-	wk.Lock()
-	defer wk.Unlock()
-	res.nTasks = wk.nTasks
-	res.concurrent = wk.concurrent
-	return nil
+//func (wk *Worker) GetStatus(_ *struct{}, res *StatusReply) error{
+//    fmt.Printf("%s: schedule() ask me the status parameters\n", wk.name)
+//    wk.Lock()
+//    defer wk.Unlock()
+//    res.nTasks = wk.nTasks
+//    res.concurrent = wk.concurrent
+//    return nil//
 
-}
+//}
 // Shutdown is called by the master when all work has been completed.
 // We should respond with the number of tasks we have processed.
 func (wk *Worker) Shutdown(_ *struct{}, res *ShutdownReply) error {
-	debug("Shutdown %s\n", wk.name)
-	wk.Lock()
-	defer wk.Unlock()
-	res.Ntasks = wk.nTasks
-	wk.nRPC = 1  //在master端调用shutdown之后work还能接受一次远端的调用
-	return nil
+    debug("Shutdown %s\n", wk.name)
+    wk.Lock()
+    defer wk.Unlock()
+    res.Ntasks = wk.nTasks
+    wk.nRPC = 1  //在master端调用shutdown之后work还能接受一次远端的调用
+    return nil
 }
 
 // Tell the master we exist and ready to work
 func (wk *Worker) register(master string) {
-	args := new(RegisterArgs)
-	args.Worker = wk.name
-	ok := call(master, "Master.Register", args, new(struct{}))
-	if ok == false {
-		fmt.Printf("Register: RPC %s register error\n", master)
-	}
+    args := new(RegisterArgs)
+    args.Worker = wk.name
+    ok := call(master, "Master.Register", args, new(struct{}))
+    if ok == false {
+        fmt.Printf("Register: RPC %s register error\n", master)
+    }
 }
 
 // RunWorker sets up a connection with the master, registers its address, and
 // waits for tasks to be scheduled. 
 func  RunWorker(MasterAddress string, me string,
-	MapFunc func(string, string) []KeyValue,
-	ReduceFunc func(string, []string) string,
-	nRPC int,
+    MapFunc func(string, string) []KeyValue,
+    ReduceFunc func(string, []string) string,
+    nRPC int,
 ) {
-	debug("RunWorker %s\n", me)
-	wk := new(Worker)
-	wk.name = me
-	wk.Map = MapFunc
-	wk.Reduce = ReduceFunc
-	wk.nRPC = nRPC
-	rpcs := rpc.NewServer() //worker启动自己的RPC服务
-	rpcs.Register(wk)
-	os.Remove(me) // only needed for "unix"
-	l, e := net.Listen("unix", me)
-	if e != nil {
-		log.Fatal("RunWorker: worker ", me, " error: ", e)
-	}
-	wk.l = l
-	wk.register(MasterAddress)
-	//调用master上的rpc服务
+    debug("RunWorker %s\n", me)
+    wk := new(Worker)
+    wk.name = me
+    wk.Map = MapFunc
+    wk.Reduce = ReduceFunc
+    wk.nRPC = nRPC
+    rpcs := rpc.NewServer() //worker启动自己的RPC服务
+    rpcs.Register(wk)
+    os.Remove(me) // only needed for "unix"
+    l, e := net.Listen("unix", me)
+    if e != nil {
+        log.Fatal("RunWorker: worker ", me, " error: ", e)
+    }
+    wk.l = l
+    wk.register(MasterAddress)
+    //调用master上的rpc服务
 
-	// DON'T MODIFY CODE BELOW
-	for {
-		wk.Lock()
-		if wk.nRPC == 0 {
-			wk.Unlock()
-			break
-		}
-		wk.Unlock()
-		conn, err := wk.l.Accept()
-		if err == nil {
-			wk.Lock()
-			wk.nRPC--
-			wk.Unlock()
-			go rpcs.ServeConn(conn)
-		} else {
-			break
-		}
-	}
-	wk.l.Close()
-	debug("RunWorker %s exit\n", me)
+    // DON'T MODIFY CODE BELOW
+    for {
+        wk.Lock()
+        if wk.nRPC == 0 {
+            wk.Unlock()
+            break
+        }
+        wk.Unlock()
+        conn, err := wk.l.Accept()
+        if err == nil {
+            wk.Lock()
+            wk.nRPC--
+            wk.Unlock()
+            go rpcs.ServeConn(conn)
+        } else {
+            break
+        }
+    }
+    wk.l.Close()
+    debug("RunWorker %s exit\n", me)
 }
